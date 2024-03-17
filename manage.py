78532c70d139b4cb02f6cd40057a311fbdc71ecd
omgitsjohn <syin18@my.bcit.ca>
2024-03-17 15:58:04 -0700

    modified manage.py so that both customer and product are seeded tot he database

--- manage.py
+++ manage.py
@@ -1,26 +1,33 @@
 import csv
 from db import db
 from app import app
-from models import Customer  # import your Customer model
+from models import Customer, Product
 
 def drop_tables():
-    with app.app_context():
-        db.drop_all()
+    db.drop_all()
 
 def create_tables():
-    with app.app_context():
-        db.create_all()
+    db.create_all()
 
-def seed_database():
-    with app.app_context():
-        with open('data/customers.csv', 'r') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                customer = Customer(name = row['name'], phone = row['phone'], balance = 0)
-                db.session.add(customer)
-            db.session.commit()
+def seed_customers():
+    with open('data/customers.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            customer = Customer(name = row['name'], phone = row['phone'], balance = 0)
+            db.session.add(customer)
+        db.session.commit()
+
+def seed_products():
+    with open('data/products.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            product = Product(name = row['name'], price = row['price'])
+            db.session.add(product)
+        db.session.commit()
 
 if __name__ == "__main__":
-    drop_tables()
-    create_tables()
-    seed_database()
+    with app.app_context():
+        drop_tables()
+        create_tables()
+        seed_customers()
+        seed_products()
