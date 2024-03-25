6bc2ca487d2ec9b30bc6a98a97a792b84d5c31d1
omgitsjohn <syin18@my.bcit.ca>
2024-03-24 22:01:31 -0700

    added relationships between the different classes

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def customer_create():
     print(request.json)
     if "name" not in request.json or "phone" not in request.json:
         return (jsonify({"error": "Name and phone are required"}), 400)
-    if isinstance(request.json["name"], str) and contains_digit(request.json["name"]):
+    if isinstance(request.json["name"], str) or contains_digit(request.json["name"]) or len(request.json["name"]) < 1:
         return (jsonify({"error": "Invalid name"}), 400)
     if contains_letter(request.json["phone"]) or not isinstance(request.json["phone"], str) or not is_valid_phone(request.json["phone"]):
         return (jsonify({"error": "Invalid phone number"}), 400)
@@ -111,7 +111,7 @@ def product_create():
     print(request.json)
     if "name" not in request.json or "price" not in request.json:
         return jsonify({"error": "Name and price are required"}, 404)
-    if not isinstance(request.json["name"], str) and not contains_letter(request.json["name"]): #some products have numbers in their name no-> contains_digit(request.json["name"])
+    if not isinstance(request.json["name"], str) or not contains_letter(request.json["name"]) or len(request.json["name"]) < 1: #some products have numbers in their name no-> contains_digit(request.json["name"])
         return jsonify({"error": "Invalid name"}, 400)
     if not isinstance(request.json["price"], float) or request.json["price"] < 0:
         return jsonify({"error": "Invalid price"}, 400)
@@ -120,7 +120,27 @@ def product_create():
     db.session.commit()
     return (jsonify({"Message": "created"}), 201)
 
+@app.route("/api/products/<int:product_id>", methods=["PUT"])
+def product_update(product_id):
+    print(request.json)
+    possible_keys = ['name', 'price', 'available']
+    product = db.get_or_404(Product, product_id)
+    if not any(key in request.json for key in possible_keys):
+        return jsonify({"error": "Invalid input"}, 400)
+    for key in possible_keys:
+        if key in request.json:
+            # product.key = request.json[key] #this will not work -> must use setattr
+            setattr(product, key, request.json[key])
+    db.session.commit()
+    return (jsonify({"Message": "product updated"}), 204)
 
+@app.route("/api/products/<int:product_id>", methods=["DELETE"])
+def product_delete(product_id):
+    product = db.get_or_404(Product, product_id) #session.execute(db.select(Product).where(Product.id == product_id)).scalar() + return 404
+    db.session.delete(product)
+    db.session.commit()
+    return (jsonify({"product deleted"}), 204)
+    
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -4,29 +4,35 @@ from sqlalchemy.orm import mapped_column, relationship
 from db import db
 #inherits from db.Model class -> otherwise will not be "registered" by Flask-SQLAlchemy
 class Customer(db.Model):
-    #primary key / sqlalch will auto-increment the integer for id
+    #primary key / sqlAlch will auto-increment the integer for id
     id = mapped_column(Integer, primary_key=True)
     name = mapped_column(String(200), nullable=False, unique=True)
     phone = mapped_column(String(20), nullable=False )
     balance = mapped_column(Numeric, nullable=False, default=0)
+    orders = relationship("Order") #only one back_populates is needed, the other is inferred
 
 class Product(db.Model):
-    #primary key / sqlalch will auto-increment the integer for id
+    #primary key / sqlAlch will auto-increment the integer for id
     id = mapped_column(Integer, primary_key=True)
     name = mapped_column(String(200), nullable=False, unique=True)
     price = mapped_column(Numeric, nullable=False)
-    available = mapped_column(Integer, nullable=False, default=0)
+    available = mapped_column(Integer, nullable=False, default=0) #number of products available
+    orders = relationship("ProductOrder")
 
 class Order(db.Model):
     id = mapped_column(Integer, nullable=False, primary_key=True)
-    customer_id = mapped_column(Integer, ForeignKey("customer.id"), nullable=False)
+    customer_id = mapped_column(Integer, ForeignKey(Customer.id), nullable=False)
+    customer = relationship("Customer", back_populates="orders")#looks up related enttiy without having to do joins (adds customer_id to the Order table)
     total = mapped_column(Numeric, nullable=False)
+    items = relationship("ProductOrder") 
 
 class ProductOrder(db.Model):
     id = mapped_column(Integer, nullable=False, primary_key=True)
-    order_id = mapped_column(Integer, ForeignKey("order.id"), nullable=False)
-    product_id = mapped_column(Integer, ForeignKey("product.id"), nullable=False)
-    quantity = mapped_column(Integer, nullable=False)
+    order_id = mapped_column(Integer, ForeignKey(Order.id), nullable=False)
+    order = relationship("Order", back_populates="items")
+    product_id = mapped_column(Integer, ForeignKey(Product.id), nullable=False)
+    product = relationship("Product", back_populates="orders")
+    quantity = mapped_column(Integer, nullable=False, default=0) #number of orders for this product
 
 
 
